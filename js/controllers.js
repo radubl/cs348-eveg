2e63e4c8390770c01d98e29f9c93876cf61b5b98
hmajid2301 <hmajid2301@gmail.com>
2016-02-23 12:20:16 +0000

    Made changes so search box works, need to change css to move positions when other boxes are hidden

--- js/controllers.js
+++ js/controllers.js
@@ -30,3 +30,31 @@ evegControllers.controller('PhoneListCtrl', ['$scope',
 		$scope.orderProp = 'age';
 
 	}]);
+
+
+//sorry radu didn't how to do teh angular way
+//but hey it works ;)
+evegControllers.controller('SearchCtrl', ['$scope',
+	function ($scope) {
+		$scope.search = '';
+		$scope.$watch('search', function (value) {
+			$(".produce-box").each(function () {
+
+				//if no value for search
+				//show all boxes
+				if (value == "") {
+					$(this).show();
+				}
+
+				else {
+					var name = $(this).find(".product-title h4 a").html().toLowerCase();
+					var check = name.indexOf(value.toLowerCase());
+
+					if (check == -1) {
+						$(this).hide();
+					}
+				}
+			})
+		});
+	}]);
+
